404531b29c7c0e6e369d6c8f8f2fb42a7f282140
MithileshPandit <59164318+MithileshPandit@users.noreply.github.com>
2022-08-15 23:24:21 +0530

    Update wordBoard.cpp

--- wordBoard.cpp
+++ wordBoard.cpp
@@ -1,70 +1,81 @@
 #include <stdio.h>
-int main()
+#define TRUE 1
+#define FALSE 0
+typedef unsigned char uint8_t;
+typedef short int16_t;
+enum js_var_type {JS_VAR_NULL, JS_VAR_UNDEFINED, JS_VAR_NAN, JS_VAR_BOOL, JS_VAR_INT16, JS_VAR_STRING, JS_VAR_ARRAY, JS_VAR_DICT};
+struct js_var {
+    enum js_var_type type;
+    int16_t number;
+    void *data;
+};
+struct js_var js_var_from(enum js_var_type type) {
+    struct js_var v;
+    v.type = type;
+    v.data = NULL;
+    return v;
+}
+
+struct word_t {
+    void * length;
+};
+struct array_word_t {
+    int16_t size;
+    int16_t capacity;
+    struct word_t ** data;
+};
+
+static uint8_t (*exist)(struct array_word_t *, struct word_t *);
+uint8_t exist_func(struct array_word_t * board, struct word_t * word)
 {
-      printf("<script language=\"JavaScript\">
-
-var nl = getNewLine()
-
-function getNewLine() {
-	var agent = navigator.userAgent
-
-	if (agent.indexOf(\"Win\") >= 0)
-		return \"\r\n\"
-	else
-		if (agent.indexOf(\"Mac\") >= 0)
-			return \"\r\"
-
- 	return \"\r\"
+    int16_t r;
+    void * c;
+    int16_t dirs[2][4];
+    uint8_t (*dfs)(int16_t, int16_t, int16_t);
+    int16_t i;
+    if (board->size == 0)
+        return FALSE;
+    r = board->size;
+    c = board->data[0]->length;
+    dirs[0][0] = -1;
+    dirs[0][1] = 0;
+    dirs[1][0] = 0;
+    dirs[1][1] = 1;
+    dirs[2][0] = 1;
+    dirs[2][1] = 0;
+    dirs[3][0] = 0;
+    dirs[3][1] = -1;
+    dfs = /* Unsupported node: (x, y, k) => { if (board[x][y] !== word[k]) { return false; } if (k === word.length - 1) { return true; } board[x][y] = '*'; for (const [dx, dy] of dirs) { const i = x + dx; const j = y + dy; if (i >= 0 && i < r && j >= 0 && j < c) { if (dfs(i, j, k + 1)) { return true; } } } board[x][y] = word[k]; return false; } */;
+     ;
+    i = 0;
+    for (;i < r;i++)
+    {
+        int16_t j;
+        j = 0;
+        for (;j < js_var_from(JS_VAR_NAN);j++)
+            if (dfs(i, j, 0))
+            return TRUE;
+    }
+    return FALSE;
 
 }
 
-pagecode = 'var exist = function (board, word) {
-    if (board.length === 0) {
-        return false
-    }
+int main(void) {
+    exist = exist_func;
+    board[0][0] = "A";
+    board[0][1] = "B";
+    board[0][2] = "C";
+    board[0][3] = "E";
+    board[1][0] = "S";
+    board[1][1] = "F";
+    board[1][2] = "C";
+    board[1][3] = "S";
+    board[2][0] = "A";
+    board[2][1] = "D";
+    board[2][2] = "E";
+    board[2][3] = "E";
+    word = "ABCCED";
+    printf("%s\n", exist(board, word) ? "true" : "false");
 
-    let r = board.length;
-    let c = board[0].length;
-    const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-    
-    const dfs = (x, y, k) => {
-        if (board[x][y] !== word[k]) {
-            return false;
-        }
-        if (k === word.length - 1) {
-            return true;
-        }
-        board[x][y] = \'*\';
-        for (const [dx, dy] of dirs) {
-            const i = x + dx;
-    const j = y + dy;
-
-            if (i >= 0 && i < r && j >= 0 && j < c) {
-                if (dfs(i, j, k + 1)) {
-                    return true;
-                }
-            }
-        }
-        board[x][y] = word[k];
-        return false;
-    }
-
-    for (let i = 0; i < r; i++) {
-        for (let j = 0; j < c; j++) {
-            if (dfs(i, j, 0)) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-board = [[\"A\",\"B\",\"C\",\"E\"],[\"S\",\"F\",\"C\",\"S\"],[\"A\",\"D\",\"E\",\"E\"]], word = \"ABCCED\"
-
-console.log(exist(board,word));'
-
-document.write(pagecode);
-
-</script>\n");
-      return 0;
+    return 0;
 }
